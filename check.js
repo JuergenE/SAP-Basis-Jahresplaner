54fe1d306419b70ab976b58a8bc5d3a5c418c132
Juergen <juergen@eifridt.net>
2026-02-26 22:23:50 +0100

    chore: remove database, debug scripts, and data dir from repo
    - Remove database.sqlite, sap-planner.sqlite from tracking
    - Remove check.js, manage-users.js utility scripts
    - Remove data/ directory
    - Update .gitignore to prevent re-adding

--- check.js
+++
@@ -1,3 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('./data/sap-planner.db');
-console.log(db.prepare('SELECT id, username, first_name, last_name, role FROM users').all());
